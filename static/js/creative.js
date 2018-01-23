49dab8927bd842b70c523c1d819a65974339b640
Caroline Ramond <caroline.ramond@iconem.com>
2018-01-23 14:51:10 +0100

    home section ok (missing assets & validation)

--- static/js/creative.js
+++ static/js/creative.js
@@ -30,8 +30,8 @@
     // Fit Text Plugin for Main Header
     $("h1").fitText(
         1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
+            minFontSize: '24px',
+            maxFontSize: '36px'
         }
     );
 
