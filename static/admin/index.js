8c6852176e0c49ed86933b360345264456210bc6
paubineau <pa@iconem.com>
2022-09-28 11:03:29 +0200

    fix

--- static/admin/index.js
+++ static/admin/index.js
@@ -2,15 +2,13 @@
 const { CMS, initCMS: init } = window;
 initCMS();
 
-// Import custom editor component from cms/editor-components.js
-// import myCustomEditorComponent from "./editor-components";
+
+import studioProjectsPreview from "./studioProjectsPreview";
 // Import NetlifyCMS JS configuration object from cms/config/index.js
 // import config from "./config";
 
-// Disable loading of the configuration from the default config.yml file
 window.CMS_MANUAL_INIT = true;
-// Initialize NetlifyCMS with the JS configuration objext
-window.CMS_CONFIGURATION = config;
-CMS.init({ config });
-// Register the custom editor component
-// CMS.registerEditorComponent(myCustomEditorComponent);
+CMS.init();
+CMS.registerPreviewStyle("/css/vendors.min.css");
+CMS.registerPreviewStyle("/css/iconem-studio.min.css");
+CMS.registerPreviewTemplate("studioProjects", studioProjectsPreview);
