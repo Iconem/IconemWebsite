f2b5586aa5d73b184d0478cfbf31fc002517058e
paubineau <pa@iconem.com>
2023-01-02 16:38:15 +0100

    fix

--- public/fr/admin/index.js
+++ public/fr/admin/index.js
@@ -3,9 +3,8 @@ const { CMS, initCMS: init } = window;
 import config from "./config/index.js";
 import projectPreview from "./config/collections/projects/preview.js";
 import teamPreview from "./config/collections/team/preview.js";
-import clientsPreview from "./config/collections/clients/preview.js";
+import clientsPreview from "./config/collections/clients/preview_clients.js";
 import partnersPreview from "./config/collections/partners/preview.js";
-import collaboratorsPreview from "./config/collections/collaborators/preview.js"
 
 initCMS({ config });
 
@@ -16,4 +15,3 @@ CMS.registerPreviewTemplate("researchProjects", projectPreview);
 CMS.registerPreviewTemplate("team", teamPreview);
 CMS.registerPreviewTemplate("partners", partnersPreview);
 CMS.registerPreviewTemplate("clients", clientsPreview);
-CMS.registerPreviewTemplate("collaborators", collaboratorsPreview);
