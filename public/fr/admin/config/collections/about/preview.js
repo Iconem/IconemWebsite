96cf2ade871e3121ce64085ba54aa52ce65b4a13
paubineau <pa@iconem.com>
2023-01-04 12:12:59 +0100

    fix

--- public/fr/admin/config/collections/about/preview.js
+++ public/fr/admin/config/collections/about/preview.js
@@ -9,9 +9,7 @@ export default createClass({
     },
     render: function () {
         this.props.document.body.backgroundColor = 'white';
-        const data = this.props.entry.toJS().data;
-        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
-        console.log(data);
+        const about = this.props.entry.toJS().about;
         const html = `
             <section id="about">
                 <section>
@@ -21,43 +19,30 @@ export default createClass({
                                 <h4 class="text-center text-uppercase text-bolder"> ABOUT ICONEM </h4>
                                 <!-- <hr> -->
                             <br>
-                            {{ with .Site.Params.about.description }}
-                                <p class="text-justify">Founded in 2013, Iconem is an innovative startup that specialises in the digitisation of endangered cultural heritage sites in 3D. We work with international organisations, national governments, local authorities, and world class museums such as UNESCO, the Aga Khan Trust for Culture, the Sultanate of Oman, the City of Paris, and the Louvre. We design site-specific architectural 3D models; large-scale urban and rural 3D models; museum exhibitions; and training for local professionals.</p>
-                            {{ end }}
+                            <p class="text-justify">Founded in 2013, Iconem is an innovative startup that specialises in the digitisation of endangered cultural heritage sites in 3D. We work with international organisations, national governments, local authorities, and world class museums such as UNESCO, the Aga Khan Trust for Culture, the Sultanate of Oman, the City of Paris, and the Louvre. We design site-specific architectural 3D models; large-scale urban and rural 3D models; museum exhibitions; and training for local professionals.</p>
                         </div>
                     </div>
                 </div>
                 </section>
                 <div class="about-row">
-                // {{ $baseUrl := .Site.Params.baseUrl}}
-                // {{ range sort .Site.Data.about "order"}}
-                // <div class="about-item">
-                //     {{ if .internal }}
-                //     <a href="{{$baseUrl}}{{ .link }}">
-                //     {{ else }}
-                //     <a href="{{ .link }}" target="_blank">
-                //     {{ end }}
-                //         <div class="about-content img-responsive">
-                //             <div class="about-content-description">
-                //                 <br />
-                //                 <br />
-                //                 <div class="text-uppercase text-center">
-                //                 {{ if eq $type "research" }}
-                //                 <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="{{ .logo.black }}" />
-                //                 {{ else if eq $type "studio" }}
-                //                 <img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="{{ .logo.white }}" />
-                //                 {{ end }}
-                //                 </div>
-                //                 <br />
-                //                 <br />
-                //                 <div class="text-justify">
-                //                 {{ index .description $lang }}
-                //                 </div>
-                //             </div>
-                //         </div>
-                //     </a>
-                // </div>
-                // {{ end }}
+                <div class="about-item">
+                    <a href="${about.link}" target="_blank">
+                        <div class="about-content img-responsive">
+                            <div class="about-content-description">
+                                <br />
+                                <br />
+                                <div class="text-uppercase text-center">
+                                <img src="${about.logo.black}" />
+                                </div>
+                                <br />
+                                <br />
+                                <div class="text-justify">
+                                ${about.description.en}
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
                 </div>
             </section>
         `;
