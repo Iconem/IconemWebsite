f418e9cfeebdc3e89ae2e5421ae468ac6e6dd48f
paubineau <pa@iconem.com>
2023-01-04 12:16:29 +0100

    fix

--- static/admin/config/collections/about/preview.js
+++ static/admin/config/collections/about/preview.js
@@ -10,6 +10,8 @@ export default createClass({
     render: function () {
         this.props.document.body.backgroundColor = 'white';
         const about = this.props.entry.toJS().about;
+        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+        console.log(about);
         const html = `
             <section id="about">
                 <section>
