3bd61565f5756a6fa5e8d5b65fbbd888bba5d911
paubineau <pa@iconem.com>
2023-01-04 12:32:15 +0100

    fix

--- public/fr/admin/config/collections/about/preview.js
+++ public/fr/admin/config/collections/about/preview.js
@@ -10,10 +10,8 @@ export default createClass({
     render: function () {
         this.props.document.body.backgroundColor = 'white';
         const about = this.props.entry.toJS().data;
-        let img = this.props.widgetsFor('img').map((el, index) => {
-            const imgSrcWidget = el && el.getIn(['widgets', 'img']);
-            return this.props.getAsset(imgSrcWidget && imgSrcWidget.props.children.props.value, imgSrcWidget && imgSrcWidget.props.children.props.field);
-        });
+        console.log(this.props);
+        let img = this.props.widgetFor('img');
 
         console.log('@@@@@@@@@@@@@@@@@@@@@@@');
         console.log(img);
