44cdb5e054520761d1ee56e8b62baad808e036da
paubineau <pa@iconem.com>
2023-08-11 10:55:41 +0200

    Update preview.js

--- static/admin/config/collections/about/preview.js
+++ static/admin/config/collections/about/preview.js
@@ -18,20 +18,24 @@ export default createClass({
             const whiteLogo = whiteLogoWidgets.props.getAsset(whiteLogoData, whiteLogoWidgets.props.field)
         }
 
-        const html = `
+        const html = order === 0 ? `
             <section id="about">
                 <section>
                 <div class="container">
                     <div class="about-row">
                         <div class="col-lg-10 col-lg-offset-1">
-                                <h4 class="text-center text-uppercase text-bolder"> ABOUT ICONEM </h4>
+                                <h4 class="text-center text-uppercase text-bolder">${about.title.en}</h4>
                                 <!-- <hr> -->
                             <br>
-                            <p class="text-justify">Founded in 2013, Iconem is an innovative startup that specialises in the digitisation of endangered cultural heritage sites in 3D. We work with international organisations, national governments, local authorities, and world class museums such as UNESCO, the Aga Khan Trust for Culture, the Sultanate of Oman, the City of Paris, and the Louvre. We design site-specific architectural 3D models; large-scale urban and rural 3D models; museum exhibitions; and training for local professionals.</p>
+                            <p class="text-justify">${about.description.en}</p>
                         </div>
                     </div>
                 </div>
                 </section>
+            </section>
+        ` :
+        `
+            <section id="about">
                 <div class="about-row">
                 <div class="about-item">
                     <a href="${about.link}" target="_blank">
